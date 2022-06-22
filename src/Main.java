e994d58335c0521a79b96c0bd0e9b49156f87f12
zesik1 <overdrivemono@gmail.com>
2022-06-22 13:00:59 +0300

    ex 1.2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the first side of the triangle - integer... :");
-        int W = input.nextInt();
-        System.out.println("Enter the second side of the triangle - integer... :");
-        int L = input.nextInt();
-        int H = (int)Math.pow(W,2) + (int)Math.pow(L,2);
-        System.out.println("Missing side of triangle is " + (Math.sqrt(H)));
+        System.out.println("Enter your 10 grades min 0 max 20 ... :");
+        int[] grades = new int[10];
+        for (int i=0; i<10 ; i++){
+            grades[i] = input.nextInt();}
+        int max = grades[0];
+        int min = grades[0];
+        int sum = 0;
+        for (int i=0; i<10; i++){
+            sum += grades[i];
+            if (grades[i] > max){
+                max = grades[i];
+            }
+            if (grades[i] < min ){
+                min = grades[i];
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Average grade of lessons is " + sum/10);
+        System.out.println("Max grade of lessons is " + max);
+        System.out.println("Min grade of lessons is " + min);
+
     }}
