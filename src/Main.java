c4e58dae1ac1c3b91b08a618418e188942cf04c1
zesik1 <overdrivemono@gmail.com>
2022-06-22 13:05:19 +0300

    ex 1.7

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("How many days you will go for vacation ? :");
-
-        int days = input.nextInt();
-        int hours = days * 24;
-        int minutes = hours * 60;
-        int seconds = minutes * 60;
-        System.out.println(" hours " + hours + " minutes " + minutes + " seconds " + seconds);
+        String username = "garonis";
+        String password = "123abc!@#";
+        System.out.println("Enter username:");
+        String usernamein = input.nextLine();
+        System.out.println("Enter password:");
+        String passwordin = input.nextLine();
+        if (usernamein.equals(username) && (passwordin.equals(password))){
+            System.out.println("Access Completed");}
+        else {
+            System.out.println("Access Denied");
+        }
     }}
