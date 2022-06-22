8c67983d03e688553961a0c29197da2aa8a8c4a1
zesik1 <overdrivemono@gmail.com>
2022-06-22 13:02:45 +0300

    ex 1.3

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter your 10 grades min 0 max 20 ... :");
-        int[] grades = new int[10];
-        for (int i=0; i<10 ; i++){
-            grades[i] = input.nextInt();}
-        int max = grades[0];
-        int min = grades[0];
-        int sum = 0;
-        for (int i=0; i<10; i++){
-            sum += grades[i];
-            if (grades[i] > max){
-                max = grades[i];
-            }
-            if (grades[i] < min ){
-                min = grades[i];
-            }
-        }
-        System.out.println(" ");
-        System.out.println("Average grade of lessons is " + sum/10);
-        System.out.println("Max grade of lessons is " + max);
-        System.out.println("Min grade of lessons is " + min);
+        System.out.println("How many days you will go for vacation ? :");
 
+        int days = input.nextInt();
+        int hours = days * 24;
+        int minutes = hours * 60;
+        int seconds = minutes * 60;
+        System.out.println(" hours " + hours + " minutes " + minutes + " seconds " + seconds);
     }}
