fa899c95c619d0b3d197b24cca252b861bc7df29
zesik1 <overdrivemono@gmail.com>
2022-06-22 12:59:30 +0300

    ex 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first side of the triangle - integer... :");
+        int W = input.nextInt();
+        System.out.println("Enter the second side of the triangle - integer... :");
+        int L = input.nextInt();
+        int H = (int)Math.pow(W,2) + (int)Math.pow(L,2);
+        System.out.println("Missing side of triangle is " + (Math.sqrt(H)));
+    }}
